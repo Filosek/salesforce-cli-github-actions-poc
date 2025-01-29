f435db745834c0765d57477d54fa642cfdead3b7
Filip Osiński <filip.osinski@cloobees.com>
2025-01-29 12:27:12 +0100

    [TEST] What will happen when 2 components fail at the same time :o?

--- force-app/main/default/lwc/accountViewer/accountViewer.js
+++ force-app/main/default/lwc/accountViewer/accountViewer.js
@@ -1,5 +1,7 @@
 import {api,LightningElement} from 'lwc';
 
+import HEADER from '@salesforce/label/c.Account_Viewer_Header';
+
 export default class AccountViewer extends LightningElement {
     @api recordId
 
