04d2cc1cd962de030141f2098cddc35c2775c9bd
Filip Osiński <filip.osinski@cloobees.com>
2025-01-29 04:34:57 +0100

    [TEST] Added more issues to JS file to check on reported format for multiple issues

--- force-app/main/default/lwc/accountViewer/accountViewer.js
+++ force-app/main/default/lwc/accountViewer/accountViewer.js
@@ -2,4 +2,9 @@ import {api,LightningElement} from 'lwc';
 
 export default class AccountViewer extends LightningElement {
     @api recordId
+
+    connectedCallback() {
+        const account;
+        console.log('xxx')
+    }
 }
