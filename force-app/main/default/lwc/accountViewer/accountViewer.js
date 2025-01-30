00708bcb6f1abb0571b1aa76f83399aa838e6478
Filip Osiński <filip.osinski@cloobees.com>
2025-01-30 23:48:15 +0100

    [TEST] Test Run for Successful validation

--- force-app/main/default/lwc/accountViewer/accountViewer.js
+++ force-app/main/default/lwc/accountViewer/accountViewer.js
@@ -1,12 +1,9 @@
 import {api,LightningElement} from 'lwc';
 
-import HEADER from '@salesforce/label/c.Account_Viewer_Header';
-
 export default class AccountViewer extends LightningElement {
-    @api recordId
+    @api recordId;
 
     connectedCallback() {
-        const account = {};
-        console.log('xxx')
+        console.log('xxx');
     }
 }
